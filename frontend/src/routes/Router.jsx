77415b41a6063ee8a1e06cb47861302b3a6af3f8
Tuan Le <leanhtuan030523@gmail.com>
2024-01-05 10:52:14 +0700

    Add theme Provider

--- frontend/src/routes/Router.jsx
+++ frontend/src/routes/Router.jsx
@@ -3,24 +3,35 @@ import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import AppStack from './AppStack';
 import AuthStack from './AuthStack'
 import AuthContext from '../context/AuthContext';
+import { useColorScheme } from 'react-native';
+import ThemeContext from '../context/ThemeContext';
 
 export default function Router() {
     const { authState } = useContext(AuthContext);
-    const theme = {
-        ...DefaultTheme,
+    const { theme } = useContext(ThemeContext);
+    
+    const darkTheme = {
         colors: {
-            ...DefaultTheme.colors,
             background: '#14171A',
-            
             text: '#E7E9EA',
             text_secondary: '#657786',
             icon: '#1DA1F2',
             icon_secondary: '#AAB8C2'
-        },
-    };
+        }
+    }
+
+    const lightTheme = {
+        colors: {
+            background: 'white',
+            text: '#E7E9EA',
+            text_secondary: '#657786',
+            icon: '#1DA1F2',
+            icon_secondary: '#AAB8C2'
+        }
+    }
 
     return (
-        <NavigationContainer theme={theme}>
+        <NavigationContainer theme={theme ==='light' ? lightTheme : darkTheme}>
             {authState.isAuth ?
                 <AppStack />
                 :
