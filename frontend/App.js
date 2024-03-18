769e0f4a337b8e0efb028a9e8d60d556197326a4
Tuan Le <leanhtuan030523@gmail.com>
2024-03-18 15:14:33 +0700

    Add socket to app

--- frontend/App.js
+++ frontend/App.js
@@ -3,10 +3,19 @@ import { AuthProvider } from './src/context/AuthContext';
 import { ThemeProvider } from './src/context/ThemeContext';
 import Router from './src/routes/Router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useEffect, useState } from 'react';
+import socket from './src/utils/socket';
 
 const queryClient = new QueryClient();
 
 export default function App() {
+	const [isConnected, setIsConnected] = useState(false);
+
+	useEffect(() => {
+		socket.connect();
+		socket.io.on('open', () => setIsConnected(true));
+		socket.io.on('close', () => setIsConnected(false));
+	}, [])
 
 	return (
 		<QueryClientProvider client={queryClient}>
