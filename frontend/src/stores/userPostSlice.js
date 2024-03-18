ec7e9159df356f4c92fa8780b5117d151338505c
Tuan Le <leanhtuan030523@gmail.com>
2024-03-18 19:07:01 +0700

    Change posts to user_posts in removeUserPost function

--- frontend/src/stores/userPostSlice.js
+++ frontend/src/stores/userPostSlice.js
@@ -2,6 +2,6 @@ export const createUserPostSlice = (set) => ({
     user_posts: [],
     setUserPosts: (user_posts) => set({ user_posts }),
     removeUserPost: (post_id) => set(state => ({
-        posts: state.posts.filter(post => post.id !== post_id)
+        user_posts: state.user_posts.filter(post => post.id !== post_id)
     }))
 });
