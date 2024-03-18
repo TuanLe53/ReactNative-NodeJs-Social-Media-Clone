eae7e77f115cfdca92d4ee1fe96dfb810b975e99
Tuan Le <leanhtuan030523@gmail.com>
2024-03-18 12:14:19 +0700

    Add QueryClientProvider

--- frontend/App.js
+++ frontend/App.js
@@ -2,16 +2,21 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { AuthProvider } from './src/context/AuthContext';
 import { ThemeProvider } from './src/context/ThemeContext';
 import Router from './src/routes/Router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 export default function App() {
 
 	return (
-		<AuthProvider>
-			<ThemeProvider>
-				<SafeAreaProvider>
-					<Router />
-				</SafeAreaProvider>
-			</ThemeProvider>
-		</AuthProvider>
+		<QueryClientProvider client={queryClient}>
+			<AuthProvider>
+				<ThemeProvider>
+					<SafeAreaProvider>
+						<Router />
+					</SafeAreaProvider>
+				</ThemeProvider>
+			</AuthProvider>
+		</QueryClientProvider>
 );
 }
