97893dc8f2df5bf5caaaabc961d3424c64f3f5cb
Tuan Le <leanhtuan030523@gmail.com>
2024-03-18 18:34:51 +0700

    Add removeUserPost function

--- frontend/src/stores/userPostSlice.js
+++ frontend/src/stores/userPostSlice.js
@@ -1,3 +1,7 @@
 export const createUserPostSlice = (set) => ({
     user_posts: [],
-})
+    setUserPosts: (posts) => set({ posts }),
+    removeUserPost: (post_id) => set(state => ({
+        posts: state.posts.filter(post => post.id !== post_id)
+    }))
+});
