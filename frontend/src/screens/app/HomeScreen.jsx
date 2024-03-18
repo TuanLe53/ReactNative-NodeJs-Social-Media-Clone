4e0a2627ba28490a05c6ef508cd2012b75db3fbc
Tuan Le <leanhtuan030523@gmail.com>
2024-03-18 15:35:42 +0700

    Add useQuery and post-state from store

--- frontend/src/screens/app/HomeScreen.jsx
+++ frontend/src/screens/app/HomeScreen.jsx
@@ -1,42 +1,43 @@
 import SafeAreaView from 'react-native-safe-area-view';
-import { useContext, useEffect, useState } from 'react';
+import { useContext } from 'react';
 import API_URL from '../../api/api_url';
 import Post from '../../components/post/Post';
 import { ActivityIndicator, ScrollView } from 'react-native';
 import AuthContext from '../../context/AuthContext';
-import socket from '../../utils/socket';
+import { useBoundStore } from '../../stores/store';
+import { useQuery } from '@tanstack/react-query';
+import { Text } from '@rneui/base';
 
 export default function HomeScreen() {
     const { authState } = useContext(AuthContext);
-    const [posts, setPosts] = useState([])
     
-    const [isConnected, setIsConnected] = useState(false);
+    const posts = useBoundStore((state) => state.posts);
+    const setPosts = useBoundStore((state) => state.setPosts);
 
-    useEffect(() => {
-        socket.connect()
-        socket.io.on('open', () => setIsConnected(true));
-        socket.io.on('close', () => setIsConnected(false));
-        
-        const fetchPosts = async () => {
-            const res = await fetch(`${API_URL.POST}`, {
-                headers: {'Authorization': 'Bearer ' + String(authState.authToken)}
-            })
-            const data = await res.json()
-            setPosts(data)
-        }
+    const fetchPosts = async () => {
+        const res = await fetch(`${API_URL.POST}`, {
+            headers: { 'Authorization': 'Bearer ' + String(authState.authToken) }
+        })
+        const data = await res.json()
+        setPosts(data)
+        return data
+    }
 
-        fetchPosts()
-    }, [])
+    const { isError, isLoading } = useQuery({
+        queryKey: ['posts'],
+        queryFn: fetchPosts,
+    });
+
+    if (isLoading) return <ActivityIndicator />
+    if (isError) return <Text>Error retrieving posts</Text>
 
     return (
         <SafeAreaView forceInset={{ top: 'always' }}>
             <ScrollView>          
-            {posts ?
+            {
                 posts.map((post) => (
                     <Post data={post} key={post.id} />
-                ))
-            :
-            <ActivityIndicator />    
+                )) 
             }
             </ScrollView>
         </SafeAreaView>
