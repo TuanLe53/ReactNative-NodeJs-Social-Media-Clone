8f591926c8c139604b78a2875111ef3eb3b8775e
Tuan Le <leanhtuan030523@gmail.com>
2024-03-19 15:36:50 +0700

    Add reset password token url

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,5 +6,6 @@ const router = express.Router();
 router.post('/login', authController.loginUser);
 router.post('/register', authController.registerUser);
 router.get('/update_token', authController.updateRefreshToken);
+router.post('/reset_password_token', authController.resetPasswordToken);
 
 module.exports = router;
