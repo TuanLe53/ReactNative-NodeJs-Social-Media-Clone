628620fb0f44a820c6514fddd6e7de778138d8c7
Tuan Le <leanhtuan030523@gmail.com>
2024-03-22 15:33:27 +0700

    Create getPostFromDB function

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,22 +1,28 @@
 const pool = require('../db/db');
 
+const getPostFromDB = async (post_id, user_id) => {
+    const postQuery = await pool.query('SELECT post.*, account.username, account.avatar FROM post LEFT JOIN account ON post.created_by = account.id WHERE post.id=$1', [post_id]);
+    const post = postQuery.rows[0];
+
+    const imagesQuery = await pool.query('SELECT img_url FROM post_image WHERE post_id = $1', [post_id]);
+    post.images = imagesQuery.rows;
+
+    const likeQuery = await pool.query('SELECt * FROM like_post WHERE post_id = $1 AND user_id = $2', [post_id, user_id]);
+    if (likeQuery.rows.length !== 0) {
+        post.is_like = true;
+    } else {
+        post.is_like = false;
+    }
+
+    return post;
+}
+
 const getPosts = async (req, res) => {
     const { id } = req.user;
 
     let posts = [];
     await Promise.all(res.paginatedResults['data'].map(async (item) => {
-        const postQuery = await pool.query('SELECT post.*, account.username, account.avatar FROM post LEFT JOIN account ON post.created_by = account.id WHERE post.id = $1 ORDER BY created_at', [item.id]);
-        const post = postQuery.rows[0];
-        
-        const imagesQuery = await pool.query('SELECT img_url FROM post_image WHERE post_id = $1', [item.id]);
-        post.images = imagesQuery.rows;
-
-        const likeQuery = await pool.query('SELECt * FROM like_post WHERE post_id = $1 AND user_id = $2', [item.id, id]);
-        if (likeQuery.rows.length !== 0) {
-            post.is_like = true;
-        } else {
-            post.is_like = false;
-        }
+        let post = await getPostFromDB(item.id, id);
 
         posts.push(post)
     }));
@@ -28,65 +34,30 @@ const getPosts = async (req, res) => {
 }
 
 const getPostsByUser = async (req, res) => {
-    const { user_id } = req.params;
     const { id } = req.user;
 
-    let posts = []
+    let posts = [];
+    await Promise.all(res.paginatedResults['data'].map(async (item) => {
+        let post = await getPostFromDB(item.id, id);
 
-    try {
-        const data = await pool.query('SELECT post.*, account.username, account.avatar FROM post LEFT JOIN account ON post.created_by = account.id WHERE created_by = $1', [user_id]);
-        if (data.rows.length === 0) {
-            return res.status(200).json(posts)
-        }
+        posts.push(post)
+    }))
 
-        await Promise.all(data.rows.map(async (post) => {
-            let data = await pool.query('SELECT img_url FROM post_image WHERE post_id = $1', [post.id])
-            post.images = data.rows
+    res.paginatedResults['data'] = posts;
 
-            let like = await pool.query('SELECT * FROM like_post WHERE post_id = $1 AND user_id = $2', [post.id, id]);
-            if (like.rows.length === 0) {
-                post.is_like = false
-            } else {
-                post.is_like = true;
-            }
-
-            posts.push(post)
-        }))
-
-        res.status(200).json(posts)
-
-    } catch (err) {
-        console.log(err)
-        res.status(500).json({error: 'Database error'})
-    }
-
+    res.status(200).json(res.paginatedResults);
 }
 
 const getPostByID = async (req, res) => {
     const { post_id } = req.params;
     const { id } = req.user;
 
-    try {
-        const postData = await pool.query('SELECT post.*, account.username, account.avatar FROM post LEFT JOIN account ON post.created_by = account.id WHERE post.id=$1', [post_id]);
-        const post = postData.rows[0];
-        if (post.length === 0) return res.status(404).json({ message: 'Post not found' });
-        
-        const imgData = await pool.query('SELECT img_url FROM post_image WHERE post_id = $1', [post_id]);
-        const images = imgData.rows;
-        post.images = images;
-        
-        const likeData = await pool.query('SELECT * FROM like_post WHERE post_id = $1 AND user_id = $2', [post_id, id])
-        if (likeData.rows.length === 0) {
-            post.is_like = false;
-        } else {
-            post.is_like = true;
-        }
+    const isExists = (await pool.query('SELECT id FROM post WHERE id = $1', [post_id])).rows[0];
+    if (isExists.length === 0) return res.status(404).json({ message: 'Post not found' });
 
-        res.status(200).json(post)
-        
-    } catch (err) {
-        res.status(500).json({ error: 'Database error' });
-    }
+    let post = await getPostFromDB(post_id, id);
+    
+    res.status(200).json(post)
 }
 
 const createPost = async (req, res) => {
