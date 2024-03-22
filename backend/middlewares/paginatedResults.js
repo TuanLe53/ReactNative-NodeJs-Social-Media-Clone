ee206ec13b890e8f812ee133a279d748c5e78ea7
Tuan Le <leanhtuan030523@gmail.com>
2024-03-22 15:35:51 +0700

    Create queries function

--- backend/middlewares/paginatedResults.js
+++ backend/middlewares/paginatedResults.js
@@ -1,16 +1,46 @@
 const pool = require('../db/db');
 
+const queries = async (model, limit, startIndex, id = null) => {
+    let countQuery = 'SELECT COUNT(*) FROM ';
+    let countQueryVars = [];
+
+    let dataQuery = 'SELECT * FROM ';
+    let dataQueryVars = [];
+
+    switch (model) {
+        case 'post':
+            countQuery += 'post'
+            dataQuery += `post LIMIT $1 OFFSET $2`
+            dataQueryVars.push(limit, startIndex)
+            break;
+        case 'postByUser':
+            countQuery += 'post WHERE created_by = $1'
+            countQueryVars.push(id)
+            dataQuery += 'post WHERE created_by = $1 LIMIT $2 OFFSET $3'
+            dataQueryVars.push(id, limit, startIndex)
+            break;
+    }
+    
+    const totalRows = (await pool.query(countQuery, countQueryVars)).rows[0].count;
+    const data = (await pool.query(dataQuery, dataQueryVars)).rows;
+
+    return { totalRows, data }
+}
+
 const paginatedResults = (model) => {
     return async (req, res, next) => {
+        const user_id = req.params.user_id ? req.params.user_id : '';
+
         const page = parseInt(req.query.page);
         const limit = parseInt(req.query.limit);
 
         const startIndex = (page - 1) * limit;
         const endIndex = page * limit;
-    
-        const totalRows = (await pool.query(`SELECT COUNT(*) FROM ${model}`)).rows[0].count;
+
+        let {totalRows, data} = await queries(model, limit, startIndex, user_id);
 
         const results = {};
+        results.data = data;
     
         if (endIndex < totalRows) {
             results.next = {
@@ -25,9 +55,6 @@ const paginatedResults = (model) => {
                 limit: limit
             }
         }
-
-        const dataQuery = await pool.query(`SELECT * FROM ${model} LIMIT $1 OFFSET $2`, [limit, startIndex]);
-        results.data = dataQuery.rows;
 
         res.paginatedResults = results;
         next()
