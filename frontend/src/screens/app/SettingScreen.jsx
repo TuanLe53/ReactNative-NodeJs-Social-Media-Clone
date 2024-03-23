83f25a7f7acdb4348515e61695b79fb05cb4207b
Tuan Le <leanhtuan030523@gmail.com>
2024-03-23 18:58:58 +0700

    Create SettingBtn

--- frontend/src/screens/app/SettingScreen.jsx
+++ frontend/src/screens/app/SettingScreen.jsx
@@ -6,55 +6,70 @@ import AuthContext from '../../context/AuthContext';
 import ThemeContext from '../../context/ThemeContext';
 import { useTheme } from '@react-navigation/native';
 
+function SettingBtn({ func, text, iconName }) {
+    const {colors} = useTheme();
+
+    return (
+        <Pressable
+            style={[styles.btn_container, { backgroundColor: colors.icon }]}
+            onPress={func}
+        >
+            <Text style={[styles.btn_text, { color: colors.text }]}>{text}</Text>
+            <Ionicons name={iconName} color={'white'} size={35} />
+        </Pressable>
+    )
+}
+
 export default function SettingScreen({navigation}) {
     const { logout, user } = useContext(AuthContext);
     const { theme, toggleTheme } = useContext(ThemeContext);
     const { colors } = useTheme();
 
+    const goToProfile = () => navigation.navigate('Profile', { id: user.id });
+    const changeTheme = () => toggleTheme(theme === 'light' ? 'dark' : 'light')
+
     return (
         <SafeAreaView forceInset={{ top: 'always' }}>
             <View>
-                <Text style={[{color: colors.text, textAlign: 'center', fontSize: 26, paddingBottom: 10}]}>Setting</Text>
+                <Text style={[{color: colors.text}, styles.title]}>Settings</Text>
             </View>
             <View>
-                <Pressable
-                    style={[styles.btn_container, {backgroundColor: colors.icon}]}
-                    onPress={() => navigation.navigate('Profile', {id: user.id})}>
-                    <Text style={[styles.btn_text, {color: colors.text}]}>Profile</Text>
-                    <Ionicons name='person-circle' color={'white'} size={35}/>
-                </Pressable>
+                <SettingBtn
+                    func={goToProfile}
+                    text='Profile'
+                    iconName='person-circle'
+                />
                 
-                <Pressable
-                    style={[styles.btn_container, {backgroundColor: colors.icon}]}
-                    onPress={() => toggleTheme(theme === 'light' ? 'dark' : 'light')}
-                >
-                    <Text style={[styles.btn_text, {color: colors.text}]}>Change Theme</Text>
-                    <Ionicons
-                        name={theme === 'light' ? 'sunny' : 'moon'}
-                        color={'white'}
-                        size={30}
-                    />
-                </Pressable>
+                <SettingBtn
+                    func={changeTheme}
+                    text='Change Theme'
+                    iconName={theme === 'light' ? 'sunny' : 'moon'}
+                />
 
-                <Pressable
-                    style={[styles.btn_container, {backgroundColor: colors.icon}]}
-                    onPress={logout}>
-                    <Text style={[styles.btn_text, {color: colors.text}]}>Log out</Text>
-                    <Ionicons name='log-out-outline' color={'white'} size={35}/>
-                </Pressable>
+                <SettingBtn
+                    func={logout}
+                    text='Log out'
+                    iconName='log-out-outline'
+                />
             </View>
         </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
+    title: {
+        textAlign: 'center',
+        fontSize: 26,
+        paddingBottom: 10    
+    },
     btn_container: {
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center',
+        marginHorizontal: 10,
         marginBottom: 5,
-        borderWidth: 1,
-        borderRadius: 9,
+        borderWidth: 2,
+        borderRadius: 12,
         padding: 10
     },
     btn_text: {
