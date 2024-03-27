05081b624b3b88568b9426f83589f92b44cb518a
Tuan Le <leanhtuan030523@gmail.com>
2024-03-27 15:54:05 +0700

    Remove comment routes

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use('/auth', require('./routes/auth'));
 app.use('/profile', require('./routes/profile'));
 app.use('/post', require('./routes/post'));
-app.use('/comment', require('./routes/comment'));
+// app.use('/comment', require('./routes/comment'));
 app.use('/chat', require('./routes/chat'));
 
 app.use('/images/avatar', express.static('storage/avatar'));
