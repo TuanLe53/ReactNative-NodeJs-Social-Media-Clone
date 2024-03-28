ec60d9261f77a3fc9ed1e1b26c440aebb96f444b
Tuan Le <leanhtuan030523@gmail.com>
2024-03-28 09:12:40 +0700

    Add commentSlice to store

--- frontend/src/stores/store.js
+++ frontend/src/stores/store.js
@@ -1,8 +1,10 @@
 import { create } from 'zustand';
 import { createPostSlice } from './postSlice';
 import { createUserPostSlice } from './userPostSlice';
+import { createCommentSlice } from './commentSlice';
 
 export const useBoundStore = create((...a) => ({
     ...createPostSlice(...a),
     ...createUserPostSlice(...a),
+    ...createCommentSlice(...a),
 }));
