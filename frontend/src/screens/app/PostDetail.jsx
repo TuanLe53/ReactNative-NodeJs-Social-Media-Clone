531d5ce315aaf7583d063a858725921a246473a0
Tuan Le <leanhtuan030523@gmail.com>
2024-04-01 09:34:21 +0700

    Usequery to fetch data instead of useffect

--- frontend/src/screens/app/PostDetail.jsx
+++ frontend/src/screens/app/PostDetail.jsx
@@ -1,72 +1,38 @@
-import { useContext, useEffect, useRef, useState } from "react";
+import { useContext } from "react";
 import SafeAreaView from 'react-native-safe-area-view';
 import Post from "../../components/post/Post";
 import API_URL from "../../api/api_url";
-import CommentReply from "../../components/comment/CommentReply";
-import Comment from "../../components/comment/Comment";
-import { ActivityIndicator, ScrollView, StyleSheet, Text, View } from "react-native";
+import { ActivityIndicator, ScrollView, StyleSheet, Text } from "react-native";
 import AuthContext from "../../context/AuthContext";
-import { useTheme } from "@react-navigation/native";
+import Comments from "../../components/comment/Comments";
+import { useQuery } from "@tanstack/react-query";
 
 export default function PostDetailScreen({ route }) {
     const { authState } = useContext(AuthContext);
     const { id } = route.params;
-    const [post, setPost] = useState();
-    const [comments, setComments] = useState([]);
-    const [replyTo, setReplyTo] = useState({
-        username: '',
-        comment_id: '',
-    });
-
-    const { colors } = useTheme();
 
     const fetchPost = async () => {
         const res = await fetch(`${API_URL.POST}/${id}`, {
             headers:{'Authorization': `Bearer ${String(authState.authToken)}`}
         })
         const data = await res.json()
-        if (res.status === 200) {
-            setPost(data)
-        } else {
-            alert(data.error)
-        }
-    }
-
-    const fetchComment = async () => {
-        const res = await fetch(`${API_URL.COMMENT}/${id}`)
-        const data = await res.json()
-        if (res.status === 200) {
-            setComments(data)
-        }
+        return data
     }
     
-    useEffect(() => {
-        fetchPost()
-        fetchComment()
-    }, [])
+    const { isError, isLoading, data } = useQuery({
+        queryKey: ['post'],
+        queryFn: fetchPost,
+    })
+
+    if (isLoading) return <ActivityIndicator />
+    if (isError) return <Text>Error</Text>
     
     return (
-        <SafeAreaView forceInset={{ top: 'always' }} style={styles.container}>
-            {post
-                ?
-                <>      
-                    <ScrollView>
-                        <Post data={post} />
-                        <View style={styles.comment_container}>
-                            <Text style={[styles.comment_header, {color: colors.text}]}>Comments</Text>
-                            {comments.map((comment) => (
-                                <Comment
-                                    data={comment}
-                                    setReplyTo={setReplyTo}
-                                    key={comment.id} />
-                            ))}
-                        </View>
-                    </ScrollView>
-                    <CommentReply post_id={id} replyTo={replyTo} setComments={setComments} setReplyTo={setReplyTo} />
-                </>
-                :
-                <ActivityIndicator />
-            }
+        <SafeAreaView forceInset={{ top: 'always' }} style={styles.container}>   
+            <ScrollView>
+                <Post data={data} />
+                <Comments post_id={id} />
+            </ScrollView>
         </SafeAreaView>
     )
 }
@@ -74,15 +40,5 @@ export default function PostDetailScreen({ route }) {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-    },
-    comment_container: {
-        borderTopColor: 'gray',
-        borderTopWidth: 1,
-        paddingHorizontal: 10,
-        paddingBottom: 55
-    },
-    comment_header: {
-        fontSize: 24,
-        fontWeight: 600
     }
 });
