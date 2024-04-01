436f8f7a61687745511cd94212e4cb3e199970de
Tuan Le <leanhtuan030523@gmail.com>
2024-04-01 09:13:03 +0700

    Delete comment slice

--- frontend/src/stores/commentSlice.js
+++
@@ -1,7 +0,0 @@
-export const createCommentSlice = (set) => ({
-    comments: [],
-    setComments: (comments) => set({ comments }),
-    addComment: (comment) => set(state => (
-        {comments: [comment, ...state.comments]}
-    ))
-});
--- frontend/src/stores/store.js
+++ frontend/src/stores/store.js
@@ -1,10 +1,8 @@
 import { create } from 'zustand';
 import { createPostSlice } from './postSlice';
 import { createUserPostSlice } from './userPostSlice';
-import { createCommentSlice } from './commentSlice';
 
 export const useBoundStore = create((...a) => ({
     ...createPostSlice(...a),
     ...createUserPostSlice(...a),
-    ...createCommentSlice(...a),
 }));
