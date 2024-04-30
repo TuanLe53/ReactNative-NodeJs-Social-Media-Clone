132017622e787734278bc238761beedef50a2a88
Tuan Le <leanhtuan030523@gmail.com>
2024-04-30 17:57:09 +0700

    Added responseListener to hanle notifications

--- frontend/App.js
+++ frontend/App.js
@@ -3,18 +3,30 @@ import { AuthProvider } from './src/context/AuthContext';
 import { ThemeProvider } from './src/context/ThemeContext';
 import Router from './src/routes/Router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import socket from './src/utils/socket';
+import * as Notifications from 'expo-notifications';
+
 
 const queryClient = new QueryClient();
 
 export default function App() {
 	const [isConnected, setIsConnected] = useState(false);
 
+	const responseListener = useRef();
+
 	useEffect(() => {
 		socket.connect();
 		socket.io.on('open', () => setIsConnected(true));
 		socket.io.on('close', () => setIsConnected(false));
+
+		responseListener.current = Notifications.addNotificationResponseReceivedListener((response) => {
+			console.log(response)
+		})
+
+		return () => {
+			Notifications.removeNotificationSubscription(responseListener.current)
+		}
 	}, [])
 
 	return (
