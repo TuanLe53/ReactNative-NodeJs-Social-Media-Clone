1d14f8c49346ef3fb6e527c2e2b8e4e799cc1355
Tuan Le <leanhtuan030523@gmail.com>
2024-04-30 17:55:07 +0700

    Add notification url

--- frontend/src/api/api_url.js
+++ frontend/src/api/api_url.js
@@ -4,6 +4,7 @@ const API_URL = {
     POST: 'http://192.168.1.138:8080/post',
     COMMENT: 'http://192.168.1.138:8080/comment',
     CHAT: 'http://192.168.1.138:8080/chat',
+    NOTIFICATION: 'http://192.168.1.138:8080/notifications',
 }
 
 export default API_URL;
